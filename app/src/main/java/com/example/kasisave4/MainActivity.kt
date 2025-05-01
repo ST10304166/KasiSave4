d14081f3c69958d8f242201b5954cf322cd2ae31
MikaRyklief <131654288+MikaRyklief@users.noreply.github.com>
2025-05-02 00:14:41 +0200

    I have added a logo and "KasiSave name to our android application."

--- app/src/main/java/com/example/kasisave4/MainActivity.kt
+++ app/src/main/java/com/example/kasisave4/MainActivity.kt
@@ -14,11 +14,11 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         // Set up the GET STARTED button click
         val getStartedButton = findViewById<Button>(R.id.button2)
