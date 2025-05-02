0fcaedb4696d8e562daaea4d9f4a7f5eaeefe2d8
MikaRyklief <131654288+MikaRyklief@users.noreply.github.com>
2025-05-02 13:58:31 +0200

    Added an income button to nav bar.

--- app/src/main/java/com/example/kasisave4/DashboardActivity.kt
+++ app/src/main/java/com/example/kasisave4/DashboardActivity.kt
@@ -42,6 +42,10 @@ class DashboardActivity : AppCompatActivity() {
 //                    startActivity(Intent(this, AnalysisActivity::class.java))
 //                    true
 //                }
+//                  R.id.nav_income -> {
+//                    startActivity(Intent(this, IncomeActivity::class.java))
+//                    true
+//                  }
                 else -> false // Handles any unexpected menu item
             }
         }
