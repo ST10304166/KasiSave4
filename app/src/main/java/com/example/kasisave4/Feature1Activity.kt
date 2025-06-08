7d40abcadb6f537068f32e4847b41b207d70da38
Steven Prince Bomela <ST10304166@vcconnect.edu.za>
2025-06-08 15:22:40 +0200

    added-skip

--- app/src/main/java/com/example/kasisave4/Feature1Activity.kt
+++ app/src/main/java/com/example/kasisave4/Feature1Activity.kt
@@ -3,6 +3,7 @@ package com.example.kasisave4
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,12 +16,14 @@ class Feature1Activity : AppCompatActivity() {
         setContentView(R.layout.activity_feature1)
 
         val mainLayout = findViewById<View>(R.id.main)
+        val skipLabel= findViewById<TextView>(R.id.skip_label)
 
         ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+skipLabel.setOnClickListener {startActivity(Intent(this, SignUpActivity::class.java))}
 
         // Navigate to Feature2Activity when the screen is tapped
         mainLayout.setOnClickListener {
