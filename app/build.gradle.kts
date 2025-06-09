db2eef8f78237095918321c9d732961ab1f8c840
Steven Prince Bomela <ST10304166@vcconnect.edu.za>
2025-06-09 21:28:46 +0200

    progress bar on dashboard is working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
 }
